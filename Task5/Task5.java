d1925b2e6545b73f0f7c3e4a6478df66bae62e55
cmo529 <carlongkit8@gmail.com>
2026-07-29 12:11:14 +0800

    Create Task5.java

---
+++ Task5/Task5.java
@@ -0,0 +1,31 @@
+package Task5;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[3];
+
+        for (int i=0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        if (numbers[0] == numbers[1] && numbers[1] == numbers[2]) {
+            System.out.println("All three numbers are equal.");
+        } else {
+            int largest = numbers[0];
+            for (int i=1; i < numbers.length; i++) {
+                if (numbers[i] > largest) {
+                    largest = numbers[i];
+                }
+            }
+            System.out.println("The largest number is: " + largest);
+        }
+
+        scanner.close();
+
+    }
+}
